6a2d18069b1a3a2ead93ae146363ec77851d9581
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2024-08-02 17:17:37 +0200

    r/src/install.libs.R: Move instead of copy executables.

--- r/src/install.libs.R
+++ r/src/install.libs.R
@@ -1,16 +1,22 @@
-execs <- c("hv", "epsilon","igd","dominatedsets","nondominated","ndsort", "eaf")
+files <- c("hv", "epsilon","igd","dominatedsets","nondominated","ndsort", "eaf")
 
-if (WINDOWS) execs <- paste0(execs, ".exe")
-if (any(file.exists(execs))) {
+file_move <- function(from, to) {
+  file.copy(from = from, to = to, overwrite = TRUE)
+  file.remove(from)
+}
+
+if (WINDOWS) files <- paste0(files, ".exe")
+if (any(file.exists(files))) {
   dest <- file.path(R_PACKAGE_DIR,  paste0('bin', R_ARCH))
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
-  file.copy(execs, dest, overwrite = TRUE)
+  file_move(files, dest)
 }
 
 files <- Sys.glob(paste0("*", SHLIB_EXT))
-dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
-dir.create(dest, recursive = TRUE, showWarnings = FALSE)
-file.copy(files, dest, overwrite = TRUE)
-if (file.exists("symbols.rds")) {
-  file.copy("symbols.rds", dest, overwrite = TRUE)
+if (any(file.exists(files))) {
+  dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
+  dir.create(dest, recursive = TRUE, showWarnings = FALSE)
+  file.copy(files, dest, overwrite = TRUE)
 }
+if (file.exists("symbols.rds"))
+  file.copy("symbols.rds", dest, overwrite = TRUE)
