6d7015678e29c12f899447e2389804f2964d5dbf
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2024-08-20 12:52:22 +0100

    c/gitversion.mk: Fix working copy detection.

--- c/gitversion.mk
+++ c/gitversion.mk
@@ -1,7 +1,7 @@
 ## Do we have git?
 ifeq ($(shell sh -c 'which git 1> /dev/null 2>&1 && echo y'),y)
   ## Is this a working copy?
-  ifeq ($(shell sh -c "LC_ALL=C git rev-parse --is-inside-work-tree 2>&1 | grep -q -F true"),true)
+  ifeq ($(shell sh -c "LC_ALL=C git rev-parse --is-inside-work-tree 2>&1 | grep -F true"),true)
      $(shell sh -c "git describe --dirty --first-parent --always --exclude '*' > git_version")
   endif
 endif
