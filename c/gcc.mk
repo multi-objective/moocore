61f6a69fd30cd17dbe4c014bc682d83f0685dc17
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-02-10 10:12:38 +0000

    * gcc.mk: Update comments.

--- c/gcc.mk
+++ c/gcc.mk
@@ -6,10 +6,10 @@ endif
 WARN_CFLAGS = -pedantic -Wall -Wextra -Wvla  -pedantic -Wconversion -Wno-sign-conversion -Wstrict-prototypes $(WERROR_FLAG)
 SANITIZERS= -fsanitize=undefined -fsanitize=address
 ifeq ($(DEBUG), 0)
-  OPT_CFLAGS ?= -O3 -flto -DNDEBUG
+  OPT_CFLAGS ?= -DNDEBUG -O3 -flto
 # Options -funroll-loops -ffast-math -msse -mfpmath=sse improve performance but are not portable.
-# Options -fstandard-precision=fast -ftree-vectorize are not well
-# supported in some versions/architectures.
+# Options -fstandard-precision=fast -ftree-vectorize are not well supported
+# in some versions/architectures.
 else
   OPT_CFLAGS += -g3 -O0 $(SANITIZERS)
 endif
