741a7345a2ac6fdf1eb49d2137ad7128f4ac0409
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-04-03 22:58:37 +0100

    python/examples/plot_metrics.py: Extend example.

--- python/examples/plot_metrics.py
+++ python/examples/plot_metrics.py
@@ -1,7 +1,7 @@
 """Computing Multi-Objective Quality Metrics
 =========================================
 
-This is an example of computing various quality metrics on two datasets.
+Several examples of computing multi-objective unary quality metrics.
 
 """
 
@@ -9,6 +9,9 @@ import numpy as np
 import moocore
 
 # %%
+# Comparing two multi-objective datasets using unary quality metrics
+# ------------------------------------------------------------------
+#
 # First, read the datasets.
 #
 
@@ -60,3 +63,65 @@ Mean IGD+:  {np.mean(uniform_igd_plus):.5f}       {np.mean(spherical_igd_plus):.
 Mean eps*:  {np.mean(uniform_epsilon):.3f}       {np.mean(spherical_epsilon):.3f}
 
 """)
+
+# %%
+# IGD and Average Hausdorff are not Pareto-compliant
+# --------------------------------------------------
+#
+# Example 4 by :cite:t:`IshMasTanNoj2015igd` shows a case where IGD gives the wrong answer.
+#
+
+ref = np.array([10, 0, 6, 1, 2, 2, 1, 6, 0, 10]).reshape(-1, 2)
+A = np.array([4, 2, 3, 3, 2, 4]).reshape(-1, 2)
+B = np.array([8, 2, 4, 4, 2, 8]).reshape(-1, 2)
+
+# %%
+# Assuming minimization of both objectives, A is better than B in terms of Pareto optimality.
+#
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_theme()
+
+df = pd.concat(
+    [
+        pd.DataFrame({"f1": x[:, 0], "f2": x[:, 1], "Set": label})
+        for x, label in zip([ref, A, B], ["Ref", "A", "B"])
+    ],
+    ignore_index=True,
+)
+ax = sns.scatterplot(data=df, x="f1", y="f2", style="Set", hue="Set", s=200)
+plt.show()
+
+# %%
+#
+# However, both :func:`moocore.igd` and :func:`moocore.avg_hausdorff_dist`
+# incorrectly measure B as better than A, whereas :func:`moocore.igd_plus` and
+# :func:`moocore.hypervolume` correctly measure A as better than B (remember
+# that hypervolume must be maximized) and :func:`moocore.epsilon_additive` measures
+# both as equally good (epsilon is weakly Pareto compliant).
+#
+
+res = pd.DataFrame(
+    dict(
+        A=[
+            moocore.igd(A, ref),
+            moocore.avg_hausdorff_dist(A, ref),
+            moocore.igd_plus(A, ref),
+            moocore.epsilon_additive(A, ref),
+            moocore.hypervolume(A, ref=10),
+        ],
+        B=[
+            moocore.igd(B, ref),
+            moocore.avg_hausdorff_dist(B, ref),
+            moocore.igd_plus(B, ref),
+            moocore.epsilon_additive(B, ref),
+            moocore.hypervolume(B, ref=10),
+        ],
+    ),
+    index=["IGD", "Hausdorff", "IGD+", "eps+", "HV"],
+)
+
+res
