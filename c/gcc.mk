41313fa5a6f066a206eb96e2165c56a914ab3935
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-04-10 11:45:33 +0100

    c/Makefile, c/gcc.mk: Allow customizing SANITIZERS when testing.

--- c/gcc.mk
+++ c/gcc.mk
@@ -4,14 +4,15 @@ ifdef WERROR
 WERROR_FLAG:=-Werror
 endif
 WARN_CFLAGS = -pedantic -Wall -Wextra -Wvla -Wconversion -Wno-sign-conversion -Wstrict-prototypes $(WERROR_FLAG)
-SANITIZERS= -fsanitize=undefined -fsanitize=address
 ifeq ($(DEBUG), 0)
+  SANITIZERS ?=
   OPT_CFLAGS ?= -DNDEBUG -O3 -flto
 # Options -funroll-loops -ffast-math -msse -mfpmath=sse improve performance but are not portable.
 # Options -fstandard-precision=fast -ftree-vectorize are not well supported
 # in some versions/architectures.
 else
-  OPT_CFLAGS ?= -g3 -O0 $(SANITIZERS)
+  SANITIZERS ?= -fsanitize=undefined -fsanitize=address
+  OPT_CFLAGS ?= -g3 -O0
 endif
 
 ifdef march
