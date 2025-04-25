7e97bd21d1280c777807f6e9fa8f0f7154b42857
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-04-25 17:53:00 +0100

    python/examples/plot_hv_approx.py: Improve description.

--- python/examples/plot_hv_approx.py
+++ python/examples/plot_hv_approx.py
@@ -1,32 +1,48 @@
 r"""Comparing methods for approximating the hypervolume
 ===================================================
 
-This example shows how to approximate the hypervolume metric of the ``CPFs.txt`` dataset using both HypE, :func:`moocore.whv_hype()`, and DZ2019, :func:`moocore.hv_approx()` for several
-values of the number of samples between :math:`10^1` and :math:`10^5`. We repeat each
-calculation 10 times to account for stochasticity.
+This example shows how to approximate the hypervolume metric of the
+``CPFs.txt`` dataset using both HypE, :func:`moocore.whv_hype()`, and DZ2019,
+:func:`moocore.hv_approx()` for several values of the number of samples between
+:math:`10^1` and :math:`10^5`.
+
 """
 
 import numpy as np
 import moocore
+
+# %%
+#
+# First calculate the exact hypervolume.
 
 ref = 2.1
 x = moocore.get_dataset("CPFs.txt")[:, :-1]
 x = moocore.filter_dominated(x)
 x = moocore.normalise(x, to_range=[1, 2])
 true_hv = moocore.hypervolume(x, ref=ref)
+
+# %%
+#
+# Next, we approximate the hypervolume using :math:`\{10^1, 10^2, \ldots,
+# 10^5\}` random samples to show the higher samples reduce the approximation
+# error. Since the approximation is stochastic, we perform 10 repetitions of
+# each computation.
+
+nreps = 10
+nsamples_exp = 5
 rng1 = np.random.default_rng(42)
 rng2 = np.random.default_rng(42)
-
 hype = {}
 dz = {}
-for i in range(1, 6):
+for i in range(1, nsamples_exp + 1):
     hype[i] = []
     dz[i] = []
-    for r in range(15):
+    for r in range(nreps):
         res = moocore.whv_hype(x, ref=ref, ideal=0, nsamples=10**i, seed=rng1)
         hype[i].append(res)
         res = moocore.hv_approx(x, ref=ref, nsamples=10**i, seed=rng2)
         dz[i].append(res)
+
 print(
     f"True HV    : {true_hv:.5f}",
     f"Mean HYPE  : {np.mean(hype[5]):.5f} [{np.min(hype[5]):.5f}, {np.max(hype[5]):.5f}]",
@@ -36,6 +52,7 @@ print(
 
 
 # %%
+#
 # Next, we plot the results.
 
 import pandas as pd
