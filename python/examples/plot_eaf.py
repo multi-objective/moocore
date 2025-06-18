873a4d0e5fd641fe0da454197a46940e0a88765e
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-06-18 13:44:08 +0100

    python/examples/plot_eaf.py: Simplify.

--- python/examples/plot_eaf.py
+++ python/examples/plot_eaf.py
@@ -15,26 +15,30 @@ import pandas as pd
 import moocore
 
 A = moocore.get_dataset("ALG_1_dat.xz")
-A[:, :2] = moocore.normalise(A[:, :2], to_range=[0, 1])
+sets = A[:, -1]
+A = A[:, :-1]
+A = moocore.normalise(A, to_range=[0, 1])
 
-aoc = moocore.apply_within_sets(A[:, :2], A[:, 2], moocore.hypervolume, ref=1)
+aoc = moocore.apply_within_sets(A, sets, moocore.hypervolume, ref=1)
 aoc = aoc.mean()
-eaf_a = moocore.eaf(A[:, :-1], sets=A[:, -1])
+eaf_a = moocore.eaf(A, sets=sets)
 eaf_a[:, 2] /= 100
 
 auc = moocore.hypervolume(eaf_a, ref=[1, 1, 0], maximise=[False, False, True])
-nruns = len(np.unique(A[:, 2]))
-print(f"Runs = {nruns}\nAUC of EAF = {auc}\nMean AOC = {aoc}\n")
+nruns = len(np.unique(sets))
+print(f"Runs = {nruns}\nAUC of EAF = {auc}\n  Mean AOC = {aoc}\n")
 
 runs = range(5, nruns + 1)
 aocs = []
 aucs = []
 for r in runs:
-    a = A[A[:, 2] <= r, :]
-    sets = a[:, -1]
-    a = a[:, :-1]
-    aoc = moocore.apply_within_sets(a, sets, moocore.hypervolume, ref=1).mean()
-    eaf_a = moocore.eaf(a, sets)
+    selection = sets <= r
+    subsets = sets[selection]
+    a = A[selection, :]
+    aoc = moocore.apply_within_sets(
+        a, subsets, moocore.hypervolume, ref=1
+    ).mean()
+    eaf_a = moocore.eaf(a, subsets)
     eaf_a[:, 2] /= 100
     auc = moocore.hypervolume(
         eaf_a, ref=[1, 1, 0], maximise=[False, False, True]
