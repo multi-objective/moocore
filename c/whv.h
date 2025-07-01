de0e21a033e93739612a81695e95a07f651e6492
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-07-01 12:02:17 +0100

    c/whv.h: Remove redundant include.

--- c/whv.h
+++ c/whv.h
@@ -1,5 +1,4 @@
 #ifndef WHV_H
 #define WHV_H
-#include <stdlib.h>
 double rect_weighted_hv2d(double *data, int n, double * rectangles, int  rectangles_nrow, const double *reference);
 #endif // WHV_H
