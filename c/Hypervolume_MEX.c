7c0529b9521304580bc742761ed585b713a18491
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-07-08 09:06:24 +0100

    c/Hypervolume_MEX.c: Guillaume approved relicensing to MPL v2.

--- c/Hypervolume_MEX.c
+++ c/Hypervolume_MEX.c
@@ -55,22 +55,16 @@
  License
  ------------
 
- Copyright (C) 2009 - 2011
+ Copyright (C) 2009 - 2011, 2025
  MEX Interface created by Guillaume Jacquenot
  guillaume.jacquenot@gmail.com
 
  Copyright (C) 2006
  Carlos M. Fonseca, Manuel Lopez-Ibanez and Luis Paquete.
 
- This program is free software (software libre); you can redistribute
- it and/or modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2 of the
- License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful, but
- WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- General Public License for more details.
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
  ------------
  History
