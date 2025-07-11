181151f76902b0f1986ba7612f69f8438161791d
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-07-11 17:45:44 +0100

    c/hv_contrib.c: Do not calculate contribution for points that do not strictly dominate the reference point.

--- c/hv_contrib.c
+++ c/hv_contrib.c
@@ -4,27 +4,37 @@
 #include <float.h>
 #include "common.h"
 #include "hv.h"
+#include "sort.h"
 
-/* Given a list of points, compute the hypervolume of each set that can be
-   obtained by removing just one point.
+/* Given a list of points, compute the exclusive hypervolume contribution of
+   point using the naive algorithm. That is, it removes one point at a time and
+   calculates hv_total - hv_i, where hv_i is the hypervolume of the set minus
+   the point i.
 
-   It does not actually compute the contribution but HV_total - HV_i,
-   where HV_total is the total HV and HV_i is the contribution of the
-   point, that is, it actually computes the HV minus the point i.
+   With hv_total=0, it computes the negated hypervolume of each subset minus
+   one point.
 */
 static void
-hv_1point_diffs (double *hvc, double *points, int dim, int size, const double * ref,
-                 const bool * uev)
+hv_1point_diffs (double *hvc, double *points, dimension_t dim, size_t size, const double * ref,
+                 const bool * uev, const double hv_total)
 {
     bool keep_uevs = uev != NULL;
+    const double tolerance = sqrt(DBL_EPSILON);
     double * tmp = MOOCORE_MALLOC(dim, double);
-    for (int i = 0; i < size; i++) {
-        memcpy (tmp, points + i * dim, sizeof(double) * dim);
-        memcpy (points + i * dim, ref, sizeof(double) * dim);
-        hvc[i] = (keep_uevs && uev[i])
-            ? 0.0
-            : fpli_hv(points, dim, size, ref);
-        memcpy (points + i * dim, tmp, sizeof(double) * dim);
+    for (size_t i = 0; i < size; i++) {
+        if (unlikely(keep_uevs && uev[i])) {
+            hvc[i] = hv_total;
+        } else if (unlikely(!strongly_dominates(points + i * dim, ref, dim))) {
+            hvc[i] = 0.0;
+        } else {
+            memcpy(tmp, points + i * dim, sizeof(double) * dim);
+            memcpy(points + i * dim, ref, sizeof(double) * dim);
+            hvc[i] = hv_total - fpli_hv(points, dim, (int) size, ref);
+            // Handle very small values.
+            hvc[i] = fabs(hvc[i]) >= tolerance ? hvc[i] : 0.0;
+            assert(hvc[i] >= 0);
+            memcpy(points + i * dim, tmp, sizeof(double) * dim);
+        }
     }
     free(tmp);
 }
@@ -37,18 +47,15 @@ hv_1point_diffs (double *hvc, double *points, int dim, int size, const double * 
    reference point.
 */
 double
-hv_contributions(double * restrict hvc, double * restrict points, int dim, int size,
+hv_contributions(double * restrict hvc, double * restrict points, int d, int n,
                  const double * restrict ref)
 {
     assert(hvc != NULL);
-    const double tolerance = sqrt(DBL_EPSILON);
-    double hv_total = fpli_hv(points, dim, size, ref);
-    hv_1point_diffs(hvc, points, dim, size, ref, NULL);
-    for (int i = 0; i < size; i++) {
-        hvc[i] = hv_total - hvc[i];
-        // Handle very small values.
-        hvc[i] = fabs(hvc[i]) >= tolerance ? hvc[i] : 0.0;
-        assert(hvc[i] >= 0);
-    }
+    ASSUME(d > 1 && d <= 32);
+    ASSUME(n > 1);
+    dimension_t dim = (dimension_t) d;
+    size_t size = (size_t) n;
+    double hv_total = fpli_hv(points, dim, (int) size, ref);
+    hv_1point_diffs(hvc, points, dim, size, ref, NULL, hv_total);
     return hv_total;
 }
