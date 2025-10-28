8465fc2bba6a6631dcca048b0ff794ff2774b56b
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-10-28 19:15:28 +0000

    * python/pyproject.toml: Set py_limited_api here for abi3 wheels.  * python/setup.py: Remove wheel setup.

--- python/setup.py
+++ python/setup.py
@@ -1,21 +1,4 @@
-# ruff: noqa: D100, D101, D102, N801
+# ruff: noqa: D100
 from setuptools import setup
-from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
-
-class bdist_wheel_abi3(_bdist_wheel):
-    def finalize_options(self):
-        _bdist_wheel.finalize_options(self)
-        # Automatically detect py_limited_api from extension modules
-        if not self.py_limited_api:
-            for ext in self.distribution.ext_modules or []:
-                if getattr(ext, "py_limited_api", False):
-                    # Use cp310 as the minimum Python version for abi3
-                    self.py_limited_api = "cp310"
-                    break
-
-
-setup(
-    cffi_modules=["src/moocore/_ffi_build.py:ffibuilder"],
-    cmdclass={"bdist_wheel": bdist_wheel_abi3},
-)
+setup(cffi_modules=["src/moocore/_ffi_build.py:ffibuilder"])
