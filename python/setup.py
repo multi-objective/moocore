d4727b765bffd36fbd660e19a906b23ea28ddf83
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-11-24 21:49:32 +0000

    python/setup.py: Force recompilation of C extensions.

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,15 @@
-# ruff: noqa: D100
+# ruff: noqa: D100, D101, D102
 from setuptools import setup
+from setuptools.command.build_ext import build_ext
 
-setup(cffi_modules=["src/moocore/_ffi_build.py:ffibuilder"])
+
+class ForceBuildExt(build_ext):
+    def build_extension(self, ext):
+        self.force = True
+        super().build_extension(ext)
+
+
+setup(
+    cmdclass={"build_ext": ForceBuildExt},
+    cffi_modules=["src/moocore/_ffi_build.py:ffibuilder"],
+)
