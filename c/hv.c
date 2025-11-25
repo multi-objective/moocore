17ad9d2759ef60274c0cd69ce19100bc95692112
Andreia P. Guerreiro <apguerreiro@users.noreply.github.com>
2025-11-25 17:02:02 +0000

    Compute a 4D hv contribution in the base case.
    Improve the 4D base case of FPL:
     * c/hv_priv.h: Remove x_aux from dlnote_t, x is const again.
     * c/hv.c:
        - Remove references to x_aux.
        - Also maintain a list sorted according to z coordinate.
        - Allocate (once) the auxiliar data structures used in the base case.
     * c/hv4d_priv.h (onec4dplusU):
        - Reconstruct the 3D base of the 4D contribution by sweeping the (sorted) z list.
        - Use an auxiliar dlnode_t list (list_aux) and an auxiliar double vector (x_aux) for modified points.
        - Take advantage of sweeping points in ascending order of the z-coordinate (continue_base_update_z_closest).
     * c/hv.c: Use ->vol instead of ->x for the auxiliary vectors.
     (fpli_hv4d): Delete unused.
     * c/hv4d_priv.h: Simplify code, fix warnings, add more asserts and comments.
     (update_bound_3d): New.

--- c/hv.c
+++ c/hv.c
@@ -96,7 +96,13 @@ fpli_setup_cdllist(const double * restrict data, dimension_t d,
     // Link head and list4d; head is not used by HV4D, so next[0] and prev[0]
     // should remain untouched.
     head->next[0] = list4d;
-    head->prev[0] = list4d; // Save it twice so we can use assert() later.
+
+    // Reserve space for auxiliar list and auxiliar x vector used in onec4dplusU.
+    // This list will store at most n-1 points.
+    dlnode_t * list_aux = (dlnode_t *) malloc(n * sizeof(*list_aux));
+    list_aux->vol = malloc(3 * n * sizeof(double));
+    head->prev[0] = list_aux;
+    list_aux->next[0] = list4d;
 
     for (i = 1; i <= n; i++) {
         // Shift x because qsort() cannot take the dimension to sort as an argument.
@@ -112,41 +118,41 @@ fpli_setup_cdllist(const double * restrict data, dimension_t d,
     for (i = 0; i < n; i++)
         scratch[i] = head + 1 + i;
 
-    int j = d_stop - 2;
-    while (true) {
+    for (int j = d_stop - 2; j >= -2; j--) {
         /* FIXME: replace qsort() by something better:
            https://github.com/numpy/x86-simd-sort
            https://github.com/google/highway/tree/52a2d98d07852c5d69284e175666e5f8cc7d8285/hwy/contrib/sort
          */
         // Sort each dimension independently.
         qsort(scratch, n, sizeof(*scratch), compare_node);
-        if (j == -1) {
-            (list4d+1)->next[1] = scratch[0];
-            scratch[0]->prev[1] = list4d+1;
+        if (j >= 0) {
+            head->r_next[j] = scratch[0];
+            scratch[0]->r_prev[j] = head;
             for (i = 1; i < n; i++) {
-                scratch[i-1]->next[1] = scratch[i];
-                scratch[i]->prev[1] = scratch[i-1];
+                scratch[i-1]->r_next[j] = scratch[i];
+                scratch[i]->r_prev[j] = scratch[i-1];
             }
-            scratch[n-1]->next[1] = list4d+2;
-            (list4d+2)->prev[1] = scratch[n-1];
-            break;
+            scratch[n-1]->r_next[j] = head;
+            head->r_prev[j] = scratch[n-1];
+        } else {
+            const int j2 = j + 2;
+            (list4d+1)->next[j2] = scratch[0];
+            scratch[0]->prev[j2] = list4d+1;
+            for (i = 1; i < n; i++) {
+                scratch[i-1]->next[j2] = scratch[i];
+                scratch[i]->prev[j2] = scratch[i-1];
+            }
+            scratch[n-1]->next[j2] = list4d+2;
+            (list4d+2)->prev[j2] = scratch[n-1];
         }
-        head->r_next[j] = scratch[0];
-        scratch[0]->r_prev[j] = head;
-        for (i = 1; i < n; i++) {
-            scratch[i-1]->r_next[j] = scratch[i];
-            scratch[i]->r_prev[j] = scratch[i-1];
-        }
-        scratch[n-1]->r_next[j] = head;
-        head->r_prev[j] = scratch[n-1];
-        j--;
         // Consider next objective (in reverse order).
         for (i = 1; i <= n; i++)
             head[i].x--;
     }
     // Reset x to point to the first objective.
-    for (i = 1; i <= n; i++)
-        head[i].x -= STOP_DIMENSION;
+    for (i = 1; i <= n; i++) {
+        head[i].x -= STOP_DIMENSION - 2;
+    }
 
     free(scratch);
 
@@ -161,8 +167,10 @@ finish:
 
 static void fpli_free_cdllist(dlnode_t * head)
 {
-    assert(head->next[0] == head->prev[0]);
+    assert(head->next[0] == head->prev[0]->next[0]);
     free_cdllist(head->next[0]); // free 4D sentinels
+    free(head->prev[0]->vol); // free x_aux (4D basecase)
+    free(head->prev[0]); // free list_aux (4D basecase)
     free(head->r_next);
     free(head->area);
     free(head);
@@ -181,6 +189,34 @@ update_bound(double * restrict bound, const double * restrict x, dimension_t dim
 }
 
 static void
+delete_4d(dlnode_t * restrict nodep)
+{
+    nodep->prev[1]->next[1] = nodep->next[1];
+    nodep->next[1]->prev[1] = nodep->prev[1];
+}
+
+static void
+delete_3d(dlnode_t * restrict nodep)
+{
+    nodep->prev[0]->next[0] = nodep->next[0];
+    nodep->next[0]->prev[0] = nodep->prev[0];
+}
+
+static void
+reinsert_4d(dlnode_t * restrict nodep)
+{
+    nodep->prev[1]->next[1] = nodep;
+    nodep->next[1]->prev[1] = nodep;
+}
+
+static void
+reinsert_3d(dlnode_t * restrict nodep)
+{
+    nodep->prev[0]->next[0] = nodep;
+    nodep->next[0]->prev[0] = nodep;
+}
+
+static void
 delete_dom(dlnode_t * restrict nodep, dimension_t dim)
 {
     ASSUME(dim > STOP_DIMENSION);
@@ -190,9 +226,8 @@ delete_dom(dlnode_t * restrict nodep, dimension_t dim)
         nodep->r_prev[d]->r_next[d] = nodep->r_next[d];
         nodep->r_next[d]->r_prev[d] = nodep->r_prev[d];
     }
-    // Dimension 4.
-    nodep->prev[1]->next[1] = nodep->next[1];
-    nodep->next[1]->prev[1] = nodep->prev[1];
+    delete_4d(nodep);
+    delete_3d(nodep);
 }
 
 static void
@@ -213,9 +248,8 @@ reinsert_nobound(dlnode_t * restrict nodep, dimension_t dim)
         nodep->r_prev[d]->r_next[d] = nodep;
         nodep->r_next[d]->r_prev[d] = nodep;
     }
-    // Dimension 4.
-    nodep->prev[1]->next[1] = nodep;
-    nodep->next[1]->prev[1] = nodep;
+    reinsert_4d(nodep);
+    reinsert_3d(nodep);
 }
 
 static void
@@ -226,15 +260,14 @@ reinsert(dlnode_t * restrict nodep, dimension_t dim, double * restrict bound)
 }
 
 static double
-fpli_hv4d(dlnode_t * restrict list, size_t c _attr_maybe_unused)
+fpli_onec4d(dlnode_t * restrict list, size_t c _attr_maybe_unused, dlnode_t * restrict the_point)
 {
     ASSUME(c > 1);
-    assert(list->next[0] == list->prev[0]);
+    assert(list->next[0] == list->prev[0]->next[0]);
     dlnode_t * restrict list4d = list->next[0];
-    // hv4dplusU() will change the sentinels for 3D, so we need to reset them.
-    reset_sentinels_3d(list4d);
-    double hv = hv4dplusU(list4d);
-    return hv;
+    dlnode_t * restrict list_aux = list->prev[0];
+    double contrib = onec4dplusU(list4d, list_aux, the_point);
+    return contrib;
 }
 
 _attr_optimize_finite_and_associative_math // Required for auto-vectorization: https://gcc.gnu.org/PR122687
@@ -429,7 +462,9 @@ hv_recursive(dlnode_t * restrict list, dimension_t dim, size_t c,
             ASSUME(dim - 1 >= STOP_DIMENSION);
             if (dim - 1 == STOP_DIMENSION) {
                 // base case of dimension 4.
-                hypera = fpli_hv4d(list, c);
+                hypera = fpli_onec4d(list, c, p1);
+                // hypera only has the contribution of p1.
+                hypera += p1_prev->area[d_stop];
             } else {
                 hypera = hv_recursive(list, dim - 1, c, ref, bound);
             }
@@ -505,7 +540,9 @@ fpli_hv_ge5d(dlnode_t * restrict list, dimension_t dim, size_t c,
         ASSUME(dim - 1 >= STOP_DIMENSION);
         if (dim - 1 == STOP_DIMENSION) {
             // base case of dimension 4.
-            hypera = fpli_hv4d(list, c);
+            hypera = fpli_onec4d(list, c, p1);
+            // hypera only has the contribution of p1.
+            hypera += p1_prev->area[d_stop];
         } else {
             hypera = hv_recursive(list, dim - 1, c, ref, bound);
         }
--- c/hv4d_priv.h
+++ c/hv4d_priv.h
@@ -101,7 +101,11 @@ static bool
 restart_base_setup_z_and_closest(dlnode_t * restrict list, dlnode_t * restrict newp)
 {
     // FIXME: This is the most expensive function in the HV4D+ algorithm.
+#ifdef HV_RECURSIVE
+    const double newx[] = { newp->x[0], newp->x[1], newp->x[2] };
+#else
     const double newx[] = { newp->x[0], newp->x[1], newp->x[2], newp->x[3] };
+#endif
     assert(list+1 == list->next[0]);
     dlnode_t * closest0 = list+1;
     dlnode_t * closest1 = list;
@@ -126,7 +130,12 @@ restart_base_setup_z_and_closest(dlnode_t * restrict list, dlnode_t * restrict n
         // if (weakly_dominates(px, newx, 3)) { // Slower
         if (p_leq_new_0 & p_leq_new_1 & p_leq_new_2) {
             //new->ndomr++;
+#ifdef HV_RECURSIVE
+            // When called by onec4dplusU, px may be just a 3-dimensional vector.
+            assert(weakly_dominates(px, newx, 3));
+#else
             assert(weakly_dominates(px, newx, 4));
+#endif
             return false;
         }
 
@@ -161,6 +170,96 @@ restart_base_setup_z_and_closest(dlnode_t * restrict list, dlnode_t * restrict n
     }
     unreachable();
 }
+
+
+/**
+   Assumes that the HV3D+ data structure is reconstructed up to z <= newp->x[2].
+   Sweeps the points in the data structure in ascending order of y-coordinate, includes newp
+   in the z list, sets up  the "closest" data of newp and, if equalz=true (i.e., all points in
+   the data structure have x[2] == newp->x[2]), it also updates the "closest" data of the point
+   that follows newp according to the lexicographic order (z,y,x).
+*/
+static inline bool
+continue_base_update_z_closest(dlnode_t * restrict list, dlnode_t * restrict newp, bool equalz)
+{
+    const double newx[] = { newp->x[0], newp->x[1], newp->x[2]};
+    assert(list+1 == list->next[0]);
+    dlnode_t * lex_prev = list+1;
+    dlnode_t * p = lex_prev->cnext[1];
+    dlnode_t * closest1;
+
+    // find newp->closest[0]
+    while (p->x[1] < newx[1]){
+        assert(p->x[2] <= newx[2]);
+        p = p->cnext[1];
+    }
+
+    if (p->x[1] != newx[1] || p->x[0] > newx[0])
+        p = p->cnext[0];
+
+    assert(lexicographic_less_2d(p->x, newx));
+    assert(lexicographic_less_2d(newx, p->cnext[1]->x));
+
+    lex_prev = p; // newp->closest[0] = lex_prev
+
+    // Check if newp is dominated.
+    if (weakly_dominates(p->x, newx, 3)) {
+        return false;
+    }
+
+    p = lex_prev->cnext[1];
+
+    // if all points in the list have z-coordinate equal to px[2]
+    if (equalz) {
+        assert(p->cnext[0]->x[1] < newx[1]);
+        assert(p->x[1] >= newx[1]);
+        // Check if newp dominates points in the list.
+        while (p->x[0] >= newx[0]) {
+            remove_from_z(p);
+            p = p->cnext[1];
+        }
+
+        assert(p->x[0] < newx[0]);
+        // update the closest of points in the list (max. 1), if needed
+        if (p != list)
+            p->closest[0] = newp;
+
+        //setup z list
+        assert(p == list || lex_prev->next[0] == p);
+        newp->prev[0] = lex_prev;
+        newp->next[0] = lex_prev->next[0];
+
+        closest1 = list;
+
+    } else {
+        //setup z list
+        newp->prev[0] = list->prev[0]->prev[0];
+        newp->next[0] = list->prev[0];
+
+         // find newp->closest[1]
+         while (p->x[0] >= newx[0]){
+             assert(p->x[2] <= newx[2]);
+             p = p->cnext[1];
+        }
+        closest1 = p;
+    }
+
+    newp->closest[0] = lex_prev;
+    newp->closest[1] = closest1;
+
+    // update cnext
+    lex_prev->cnext[1] = newp;
+    newp->cnext[0] = lex_prev;
+    newp->cnext[1] = p;
+    p->cnext[0] = newp;
+
+    //update z list
+    newp->next[0]->prev[0] = newp;
+    newp->prev[0]->next[0] = newp;
+
+    return true;
+}
+
 
 // FIXME: This is very similar to the loop in hvc3d_list() but it doesn't use p->last_slice_z
 static double
@@ -219,7 +318,7 @@ one_contribution_3d(dlnode_t * restrict newp)
 
 /* Compute the hypervolume indicator in d=4 by iteratively computing the one
    contribution problem in d=3. */
-static double
+static inline double
 hv4dplusU(dlnode_t * list)
 {
     assert(list+2 == list->prev[0]);
@@ -247,5 +346,135 @@ hv4dplusU(dlnode_t * list)
     }
     return hv;
 }
+
+
+static inline void
+update_bound_3d(double * restrict dest, const double * restrict a, const double * restrict b)
+{
+    for (int i = 0; i < 3; i++)
+        dest[i] = MAX(a[i], b[i]);
+}
+
+// FIXME: Move this function to hv.c
+#ifdef HV_RECURSIVE
+/**
+   Compute the hv contribution of "the_point" in d=4 by iteratively computing
+   the one contribution problem in d=3. */
+static inline double
+onec4dplusU(dlnode_t * restrict list, dlnode_t * restrict list_aux,
+            dlnode_t * restrict the_point)
+{
+    // FIXME: This is never triggered.
+    assert(the_point->ignore < 3);
+    if (the_point->ignore >= 3) {
+        return 0;
+    }
+
+    assert(list+2 == list->prev[0]);
+    assert(list+2 == list->prev[1]);
+    assert(list+1 == list->next[1]);
+
+    dlnode_t * newp = (list+1)->next[0];
+    dlnode_t * z_first = newp;
+    const dlnode_t * const last = list+2;
+    dlnode_t * z_last = last->prev[0];
+
+
+    double * x_aux = list_aux->vol;
+    dlnode_t * newp_aux = list_aux+1; // list_aux is a sentinel
+
+    reset_sentinels_3d(list);
+    restart_list_y(list);
+
+    // FIXME: Would it be possible to remove the_point so we don't need to test it?
+
+    const double * the_point_x = the_point->x;
+    if ((list+1)->next[1] != the_point) {
+        bool done_once = false;
+        // PART 1: Setup 3D base (if there are any points below the_point_x[3])
+        assert(newp != last);
+        do {
+            const double * newpx = newp->x;
+            if (newpx[3] <= the_point_x[3] && newp != the_point && newp->ignore < 3){
+                if (newpx[0] <= the_point_x[0] && newpx[1] <= the_point_x[1] && newpx[2] <= the_point_x[2]) {
+                    the_point->ignore = 3;
+                    // Restore modified links (z list).
+                    (list+1)->next[0] = z_first;
+                    (list+2)->prev[0] = z_last;
+                    return 0;
+                }
+
+                // x_aux is the coordinate-wise maximum between newpx and the_point_x.
+                update_bound_3d(x_aux, newpx, the_point_x);
+                newp_aux->x = x_aux;
+                x_aux += 3;
+                bool equalz = newp_aux->x[2] == the_point_x[2] && done_once;
+                if (continue_base_update_z_closest(list, newp_aux, equalz)) {
+                    newp_aux++;
+                    done_once = true;
+                }
+            }
+            newp = newp->next[0];
+        } while (newp != last);
+    }
+    newp = the_point->next[1];
+    while (newp->x[3] <= the_point_x[3])
+        newp = newp->next[1];
+
+    dlnode_t * tp_prev_z = the_point->prev[0];
+    dlnode_t * tp_next_z = the_point->next[0];
+    // FIXME: Why do we ignore the return value of this function?
+    bool res = restart_base_setup_z_and_closest(list, the_point);
+    assert(res);
+    double volume = one_contribution_3d(the_point);
+    the_point->prev[0] = tp_prev_z;
+    the_point->next[0] = tp_next_z;
+
+    assert(volume > 0);
+    double height = newp->x[3] - the_point_x[3];
+    // It cannot be zero because we exited the loop above.
+    assert(height > 0);
+    double hv = volume * height;
+
+    // PART 2: Update the 3D contribution.
+    while (newp != last) {
+        const double * newpx = newp->x;
+        if (newpx[0] <= the_point_x[0] && newpx[1] <= the_point_x[1] && newpx[2] <= the_point_x[2])
+            break;
+
+        if (newp->ignore < 3) {
+            // x_aux is the coordinate-wise maximum between newpx and the_point_x.
+            update_bound_3d(x_aux, newpx, the_point_x);
+            newp_aux->x = x_aux;
+            x_aux += 3;
+            if (restart_base_setup_z_and_closest(list, newp_aux)) {
+                // newp was not dominated by something else.
+                double newp_v = one_contribution_3d(newp_aux);
+                assert(newp_v > 0);
+                volume -= newp_v;
+
+                add_to_z(newp_aux);
+                update_links(list, newp_aux);
+            }
+            // FIXME: If the above returned false, then newp_aux was not used
+            // so we could re-use it, no?
+            newp_aux++;
+        }
+        // FIXME: If newp was dominated, can we accumulate the height and update
+        // hv later? AG: Yes
+        height = newp->next[1]->x[3] - newpx[3];
+        assert(height >= 0);
+        hv += volume * height;
+
+        newp = newp->next[1];
+    }
+
+    // Restore z list
+    (list+1)->next[0] = z_first;
+    (list+2)->prev[0] = z_last;
+
+    return hv;
+}
+#endif // HV_RECURSIVE
 
 #endif // _HV4D_PRIV_H
