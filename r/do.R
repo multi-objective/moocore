d40e077a3ce79064e32e6ad1fa12e9b2cf62fcbb
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-12-02 00:27:59 +0000

    * .github/workflows/R.yml (coverage): Handle files under c/.  * r/Makefile (covr): Call do.R.  * r/cleanup: Remove files generated by coverage.  * r/do.R: Handle coverage.

--- r/do.R
+++ r/do.R
@@ -11,6 +11,17 @@ if (argv[1] == "submit") {
   cli::cat_rule("Info", col = "cyan")
   cli::cli_inform(c(i = "Path {.file {built_path}}", i = "File size: {prettyunits::pretty_bytes(size)}"))
   cli::cat_line()
+} else if (argv[1] == "coverage") {
+  Sys.setenv(NOT_CRAN='true')
+  options(browser="google-chrome",
+    covr.gcov_additional_paths="c",
+    covr.filter_non_package=FALSE)
+  coverage <- withr::with_makevars(c(MOOCORE_DEBUG='1', LTO_OPT=''),
+    covr::package_coverage(type='all', commentDonttest=FALSE, commentDontrun=FALSE, quiet=FALSE, clean=TRUE, pre_clean=TRUE,
+      relative_path=normalizePath(file.path(getwd(),".."))))
+  print(coverage)
+  covr::report(coverage, browse=TRUE)
+  covr::to_cobertura(coverage)
 } else {
-  stop("Unknown command")
+  stop("Unknown command", argv[1])
 }
