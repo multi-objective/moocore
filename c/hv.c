6cf440b783df52717a97f6fefa5c4fba1105262d
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-12-03 11:32:51 +0000

    hv.c: Minor simplification.

--- c/hv.c
+++ c/hv.c
@@ -530,18 +530,14 @@ fpli_hv_ge5d(dlnode_t * restrict list, dimension_t dim, size_t c,
     // FIXME: This is never used?
     // bound[d_stop] = p0->x[dim];
     reinsert_nobound(p0, dim);
-    p1 = p0;
-    dlnode_t * p1_prev = p0->r_prev[d_stop - 1];
-    p0 = p0->r_next[d_stop - 1];
-    c++;
 
-    assert(c > 1);
     while (true) {
-        // FIXME: This is not true in the first iteration if c > 1 previously.
-        //assert(p0 == p1->r_prev[d_stop]);
-        assert(p1_prev == p1->r_prev[d_stop - 1]);
+        dlnode_t * p1_prev = p0->r_prev[d_stop - 1];
+        p1 = p0;
+        p0 = p0->r_next[d_stop - 1];
         p1->vol[d_stop] = hyperv;
         assert(p1->ignore == 0);
+        c++;
         double hypera;
         if (dim - 1 == STOP_DIMENSION) {
             /*---------------------------------------
@@ -580,10 +576,6 @@ fpli_hv_ge5d(dlnode_t * restrict list, dimension_t dim, size_t c,
         // bound[d_stop] = p0->x[dim];
         // FIXME: Does updating the bound here matters?
         reinsert(p0, dim, bound);
-        p1 = p0;
-        p1_prev = p0->r_prev[d_stop - 1];
-        p0 = p0->r_next[d_stop - 1];
-        c++;
     }
 }
 
