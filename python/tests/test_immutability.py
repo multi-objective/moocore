6b6bc0d7fe413385e7ae28958c253a84bd8932dc
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2025-12-07 20:51:31 +0000

    * python/tests/test_immutability.py: Increase coverage.

--- python/tests/test_immutability.py
+++ python/tests/test_immutability.py
@@ -8,7 +8,7 @@ def test_immutable_call(immutable_call):
 
     def bad(x):
         x[0] = 42  # Accidental mutation
-        return x.sum()
+        return x.sum()  # nocov
 
     x = np.array([1, 2, 3])
     with pytest.raises(AssertionError):
@@ -20,7 +20,7 @@ def test_partial_immutability_positional(immutable_call):
 
     def bad(x, y):
         x[0] = 99  # SHOULD be caught
-        return x.sum() + y.sum()
+        return x.sum() + y.sum()  # nocov
 
     def good(x, y):
         y[0] = 88  # allowed
@@ -39,8 +39,8 @@ def test_partial_immutability_keyword(immutable_call):
     """Check partial immutable_call works."""
 
     def bad(a, b):
-        b[0] = 111  # allowed
-        return a + b
+        b[0] = 111  # not allowed
+        return a + b  # nocov
 
     def good(a, b):
         a[0] = 111  # allowed
@@ -61,18 +61,50 @@ def test_nested_structures(immutable_call):
     def f1(d):
         d["x"][2]["w"][0] = 99  # forbidden
 
-    def f2(d):
-        d["x"][2]["w"] = np.array([5, 6])  # forbidden
+    with pytest.raises(AssertionError):
+        immutable_call(f1, data)
 
     def f3(d):
         d["x"][2]["w"] = np.array([5.0, 6.0])  # forbidden
 
     with pytest.raises(AssertionError):
-        immutable_call(f1, data)
+        immutable_call(f3, data)
 
-    # FIXME: This doesn't work!
-    # with pytest.raises(AssertionError):
-    #     immutable_call(f2, data)
+    def f4(d):
+        d["x"] = (1, 2, {"w": np.array([5, 6])})  # forbidden
 
     with pytest.raises(AssertionError):
-        immutable_call(f3, data)
+        immutable_call(f4, data)
+
+    def f5(d):
+        d["x"] = [1, 2]  # forbidden
+
+    with pytest.raises(AssertionError):
+        immutable_call(f5, data)
+
+    def f6(d):
+        d["y"] = [1, 2, {"w": np.array([5, 6])}]  # forbidden
+
+    with pytest.raises(AssertionError):
+        immutable_call(f6, data)
+
+    data = [1, 2]
+
+    def f7(d):
+        d.append(3)
+
+    with pytest.raises(AssertionError):
+        immutable_call(f7, data)
+
+
+@pytest.mark.xfail
+def test_identity(immutable_call):
+    """We should forbid replacing an element with a new object that looks the same."""
+    data = {"x": [1, 2, {"w": np.array([5, 6])}]}
+
+    # FIXME: This doesn't work!
+    def f2(d):
+        d["x"][2]["w"] = np.array([5, 6])  # forbidden
+
+    with pytest.raises(AssertionError):
+        immutable_call(f2, data)
