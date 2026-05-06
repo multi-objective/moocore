a9ff692c9fbe5a1200bd6dc4c51f080f9f416668
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2026-05-06 21:57:34 +0100

    * r/R/zzz.R (.libmoocore_constants): New. Initialize it onLoad.  * r/R/nondominated.R, r/R/hv.R: Use it in documentation.  * r/src/Rmoocore.c (libmoocore_constants): New.  * r/src/init.c, r/src/init.h (DECLARE_CALL_VOID): New.

--- r/R/zzz.R
+++ r/R/zzz.R
@@ -1,4 +1,7 @@
-.onLoad <- function(lib, pkg){
+.libmoocore_constants <- NULL
+
+.onLoad <- function(lib, pkg) {
+  .libmoocore_constants <<-  .Call("libmoocore_constants")
   Rdpack::Rdpack_bibstyles(package = pkg, authors = "LongNames")
   invisible(NULL)
 }
